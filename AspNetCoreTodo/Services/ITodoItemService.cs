fed0c91bd8580bff40892514ea049b9275c17efb
Jessica Bohn <jessicabohn74@gmail.com>
2020-09-04 10:18:56 -0300

    ignore

--- AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -7,9 +7,12 @@ namespace AspNetCoreTodo.Services
 {
     public interface ITodoItemService
     {
-    Task<TodoItem[]> GetIncompleteItemsAsync();
+        Task<TodoItem[]> GetIncompleteItemsAsync();
 
-    Task<bool> AddItemAsync(TodoItem newItem);
+        Task<bool> AddItemAsync(TodoItem newItem);
+
+        Task<bool> MarkDoneAsync(Guid id);
+
     }
 
-} 
+}
