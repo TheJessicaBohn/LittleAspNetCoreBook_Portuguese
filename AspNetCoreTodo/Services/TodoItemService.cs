fb5c9039c05edc88a836d1e0abd76fe771e20d8c
Jessica Bohn <jessicabohn74@gmail.com>
2020-09-04 10:10:44 -0300

    Revert "update"
    This reverts commit 6c26cd99038123307446d6a1ed907b4b8c0b703a.

--- AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/Services/TodoItemService.cs
@@ -8,46 +8,32 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreTodo.Services
 {
-    public class TodoItemService : ITodoItemService
-    {
-        private readonly ApplicationDbContext _context;
-        public TodoItemService(ApplicationDbContext context)
+        public class TodoItemService : ITodoItemService    
         {
-            _context = context;
-        }
-
+            private readonly ApplicationDbContext _context;
+            public TodoItemService(ApplicationDbContext context) 
+            {           
+                 _context = context;       
+            }
+           
         public async Task<bool> AddItemAsync(TodoItem newItem)
         {
-            newItem.Id = Guid.NewGuid();
-            newItem.IsDone = false;
-            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+        newItem.Id = Guid.NewGuid();
+        newItem.IsDone = false;
+        newItem.DueAt = DateTimeOffset.Now.AddDays(3);
 
-            _context.Items.Add(newItem);
+        _context.Items.Add(newItem);
 
-            var saveResult = await _context.SaveChangesAsync();
-            return saveResult == 1;
+       var saveResult = await _context.SaveChangesAsync();
+      return saveResult == 1;
+      }
+
+            public async Task<TodoItem[]> GetIncompleteItemsAsync()       
+            {
+             return await _context.Items               
+              .Where(x => x.IsDone == false)           
+              .ToArrayAsync();        
+            }    
+        
         }
-
-        public async Task<TodoItem[]> GetIncompleteItemsAsync()
-        {
-            return await _context.Items
-             .Where(x => x.IsDone == false)
-             .ToArrayAsync();
-        }
-
-        public async Task<bool> MarkDoneAsync(Guid id)
-        {
-            var item = await _context.Items
-                .Where(x => x.Id == id)
-                .SingleOrDefaultAsync();
-
-            if (item == null) return false;
-
-            item.IsDone = true;
-
-            var saveResult = await _context.SaveChangesAsync();
-            return saveResult == 1; // One entity should have been updated
-        }
-
-    }
 }
